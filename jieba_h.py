d166ec6c0e8745be899ab114918db801a9028902
stagebo <1254117589@qq.com>
2018-01-09 10:46:42 +0800

    2018-1-9 9:32:52

--- jieba_h.py
+++ jieba_h.py
@@ -1,20 +1,38 @@
 
 import tornado.web
 import pyrestful.rest
-
+import json
+import jieba
 
 from pyrestful.rest import get, post, put, delete
 from pyrestful import mediatypes
 
 
-class Book(object):
-    isbn = int
-    title = str
 
 class JiebaHandler(pyrestful.rest.RestHandler):
 
     @get(_path="/jieba")
     def getpage(self):
+        print(self.get_query_argument("t"))
         self.render("templates/jieba/index.html")
 
 
+    @get(_path="/jieba/split")
+    def post_data(self):
+        # self.write("123")
+        err = json.dumps({
+                "ret": "0",
+                "msg": "数据结构错误",
+            })
+        cont = self.get_query_argument("cont")
+        if not cont or cont == "":
+            self.render("templates/jieba/index.html")
+            self.write(err)
+        seg_list = jieba.cut(cont, cut_all=True)
+        data = "/".join(seg_list)
+
+        self.write(json.dumps({
+            "ret": "1",
+            "msg": "",
+            "data": data
+            }))
--- webrest.py
+++ webrest.py
@@ -6,28 +6,26 @@ from pyrestful import mediatypes
 from pyrestful.rest import get, post, put, delete
 from jieba_h import JiebaHandler
 
+class Application():
+    def create(self):
+        settings={}
+        app = pyrestful.rest.RestService([
+            JiebaHandler
+           ,StaticHandler
+        ])
 
-class UploadFileHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('''
-                  <html>
-                    <head><br>                      <title>Upload File</title><br>                    </head>
-                    <body>
-                      <form action='file' enctype="multipart/form-data" method='post'>
-                        <input type='file' name='file'/><br/>
-                        <input type='submit' value='submit'/>
-                      </form>
-                    </body>
-                  </html>
-                  ''')
+        return app
+class StaticHandler(pyrestful.rest.RestHandler):
+
+    @get(_path="/static/plugins/{plugin}/{file}")
+    def getresource(self, plugin, file):
+        self.render("static/plugins/%s/%s"%(plugin, file))
+
 if __name__ == '__main__':
     try:
         print("Start the service")
-        app = pyrestful.rest.RestService([
-            JiebaHandler,
-
-        ])
-        app.listen(8080)
+        app = Application().create()
+        app.listen(8888)
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
         print("\nStop the service")
