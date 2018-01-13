676b589408b69c9265354aa52d66adc8a0aad8c0
stagebo <1254117589@qq.com>
2018-01-13 15:54:32 +0800

    script auto push, time 2018-01-13 15:54:32

--- handler_admin.py
+++ handler_admin.py
@@ -11,11 +11,22 @@ import logging
 from pyrestful.rest import get, post, put, delete
 from pyrestful import mediatypes
 import traceback
-
+import gl
 
 class AdminHandler(pyrestful.rest.RestHandler):
 
     @get(_path="/admin")
     def getpage(self):
-        self.render("admin/index.html")
+        hq_cookie = self.get_cookie('xr_cookie')  # 获取浏览器cookie
+        session = gl.gl_session.get(hq_cookie, None)  # 将获取到的cookie值作为下标，在数据字典里找到对应的用户信息字典
+        if not session:  # 判断用户信息不存在
+            self.redirect("/admin/login")
+        else:
+            if session.get('is_login', None) == True:  # 否则判断用户信息字典里的下标is_login是否等于True
+                self.render("admin/index.html")
+            else:
+                self.redirect("/admin/login")
 
+    @get(_path="/admin/login")
+    def login(self):
+        self.render("admin/login.html")
