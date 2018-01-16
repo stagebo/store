0475d21dec58152bc4fc7fbfd3c5f7d1861df3ad
wanyongbo <1254117589@qq.com>
2018-01-16 16:01:16 +0800

    script auto push, time 2018-01-16 16:01:15

--- restart.py
+++ restart.py
@@ -14,4 +14,5 @@ def kill_pyweb():
         elif "tale" in proc and "java" in proc:
             print(proc, pid)
 if __name__ == "__main__":
-    os.system("bash start.sh")
+    kill_pyweb
+    #os.system("bash start.sh")
