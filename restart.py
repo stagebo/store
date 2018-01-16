5de5fa1358064203c97f59ced8a965571e76c76f
wanyongbo <1254117589@qq.com>
2018-01-16 16:00:13 +0800

    script auto push, time 2018-01-16 16:00:13

---
+++ restart.py
@@ -0,0 +1,17 @@
+import os
+
+#lines connect to popen_file's read
+def kill_pyweb():
+    lines = os.popen('ps -ef')
+    for line in lines:
+        if line.find("python3") == -1 and line.find("java") == -1:
+            continue
+        vars = line.split()
+        pid = vars[1]  # get pid
+        proc = ' '.join(vars[7:])  # get proc description
+        if "web.py" in proc and "python" in proc:
+            os.system("kill %s"%pid)
+        elif "tale" in proc and "java" in proc:
+            print(proc, pid)
+if __name__ == "__main__":
+    os.system("bash start.sh")
