eddda7c7faa5b51e7c7c1831f78a7a371bbe0e35
wanyongbo <1254117589@qq.com>
2018-01-25 15:04:55 +0800

    script auto push, time 2018-01-25 15:04:55

--- handler_message.py
+++ handler_message.py
@@ -18,7 +18,30 @@ import time  # 导入时间模块
 import sys
 
 class MessageHandler(pyrestful.rest.RestHandler):
+    def err(self,s):
+        return {
+            "ret":0,
+            "msg":s
+        }
+
     @get(_path="/message")
     def get_index(self):
         self.render("message/index.html")
 
+    @post(_path="/message/submit_message",_produces=mediatypes.APPLICATION_JSON)
+    def add_message(self):
+        message = self.get_body_argument("message", None)
+        nickname = self.get_body_argument("nickname", None)
+
+        if not nickname or nickname == "":
+            nickname = "匿名用户"
+        if not message or message == "":
+            return self.err("");
+
+        print(message,nickname)
+        return {
+            '昵称': nickname,
+            '留言内容':message,
+            'message':",能正确接收数据但是还没有做入库处理！"
+
+        }
