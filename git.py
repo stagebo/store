0727d432a7159f79346b7e1ae57526aac8cd42fe
wanyongbo <1254117589@qq.com>
2018-01-26 15:12:56 +0800

    script auto push, time 2018-01-26 15:12:56

--- git.py
+++ git.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     win_push("git add .")
     win_push('git commit -m "script auto push, time %s"'%datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     win_push('git push -u origin master')
-    try:
-        requests.get("http://stagebo.xyz/admin/restart")
-    except:
-        print("this code should be update to www.stagebo.xyz!")
+    # try:
+    #     requests.get("http://stagebo.xyz/admin/restart")
+    # except:
+    #     print("this code should be update to www.stagebo.xyz!")
