c09fbd5d2dd31436b57eb711ebb4f2d1466a5c66
stagebo <1254117589@qq.com>
2018-03-09 11:47:40 +0800

    123

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS t_jieba;
 CREATE TABLE t_jieba
 (
     f_time datetime NOT NULL,
-    f_content text NOT NULL,
+    f_content nvarchar(2048) NOT NULL,
     f_ip nvarchar(64),
     f_city nvarchar(256)
 );
